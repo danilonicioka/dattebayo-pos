82feee39ea6d7f43a7d3dbaefc24a5dc405c667a
Danilo Nicioka <danilo.nicioka@tjpa.jus.br>
2026-02-03 12:38:30 -0300

    gerenciamento de estoque

---
+++ src/backend/src/main/java/com/dattebayo/pos/dto/MenuItemDTO.java
@@ -0,0 +1,21 @@
+package com.dattebayo.pos.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MenuItemDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Double basePrice;
+    private String category;
+    private Boolean available;
+    private Boolean applyMarkup;
+    private Double manualPrice;
+    private Boolean manualPriceEnabled;
+    private Integer stockQuantity;
+    private List<MenuItemVariationDTO> variations;
+}

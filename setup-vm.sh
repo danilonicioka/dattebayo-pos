193744fc08dad3c56e69e56f4c6741c17adf3d30
Danilo Nicioka <danilo.nicioka@tjpa.jus.br>
2026-03-07 03:32:29 -0300

    setup vm script

--- setup-vm.sh
+++ setup-vm.sh
@@ -10,34 +10,32 @@ echo "🔄 Updating package list..."
 sudo apt-get update
 
 echo "📦 Installing prerequisites..."
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-echo "🔑 Adding Docker's official GPG key..."
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --always-export
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "📂 Setting up the Docker repository..."
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-echo "🔄 Updating package list again..."
-sudo apt-get update
+sudo apt update
 
 echo "🐳 Installing Docker Engine, CLI, and Compose..."
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 echo "👤 Adding current user to the 'docker' group..."
 sudo usermod -aG docker $USER
 
 echo "🚀 Starting services..."
-chmod +x ./restart-services.sh
 ./restart-services.sh
 
 echo ""
